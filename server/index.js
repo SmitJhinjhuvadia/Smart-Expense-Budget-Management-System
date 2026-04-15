ca12c62033be2be0373e9e2b8ef76ef9d78fe78f
SmitJhinjhuvadia <smit.jhinjhuvadia129212@marwadiuniversity.ac.in>
2026-04-15 21:05:00 +0530

    final login fix

--- server/index.js
+++ server/index.js
@@ -8,8 +8,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://smart-expense-budget-management-system-iee3fudq3.vercel.app"
+  ],
+  credentials: true
+}));app.use(express.json());
 
 // ─── CONNECT TO MONGODB ──────────────────────────────────────────
 mongoose.connect(process.env.MONGO_URI, { family: 4 })
